92af18ab894c794d93ab81afbdbef8755c8b9a96
Nitesh-loop <nitesh166k@gmail.com>
2022-06-11 17:20:14 +0530

    changes

--- DSA_questions/src/Array_part1/RemoveDuplicate.java
+++ DSA_questions/src/Array_part1/RemoveDuplicate.java
@@ -3,10 +3,12 @@ package Array_part1;
 public class RemoveDuplicate {
 	
 	public static int removeDuplicate (int[] a) {
-		
+		//{0,(1),2,(3),(4),5,6}
+		//{2,(2),4,(4),(5),8,8};
 		int n=a.length;
 		int j=0;
-		for(int i=0; i<n-1; i++) {
+		int i;
+		for( i=0; i<n-1; i++) {
 			if(a[i] != a[i+1]) {
 				a[j++] = a[i];
 			}
@@ -16,13 +18,15 @@ public class RemoveDuplicate {
 	}
 	
 	public static void main(String[] args) {
-		int[] a = {2,2,2,3,4,4,7};
+		int[] a = {2,2,4,4,5,8,8};
 		
 		int index = removeDuplicate(a);
-
+	
 		for(int i=0; i<index; i++) {
+			
 			System.out.print(a[i]+" ");
 		}
+		
 	}
 
 }
